516b3e58bd7c3ea0531ae5f699056cfdc6f09835
Anna Kukushkina <deathdarco@rambler.ru>
2020-06-20 16:10:30 +0300

    iterator task 1 solved

---
+++ data-structures/src/main/java/BackwardArrayIt.java
@@ -0,0 +1,26 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BackwardArrayIt implements Iterator<Integer> {
+        private int[] data;
+        private int point = 0;
+
+        public BackwardArrayIt(int[] data) {
+            this.data = data;
+            point = data.length - 1;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return point >= 0;
+        }
+
+        @Override
+        public Integer next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return data[point--];
+        }
+    }
+
